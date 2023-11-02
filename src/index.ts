e3a481b02331f7ee852628cb53e9278da05ec37c
alexraskin <alexraskin@fastmail.fm>
2023-11-01 23:06:33 -0700

    use more context

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hono, Context } from 'hono'
 import { Ai } from '@cloudflare/ai'
 
 import indexHtml from './public/index.html'
+import { dwight_context } from './constants'
 
 type Bindings = {
   AI: Ai
@@ -19,7 +20,7 @@ app.post('/', async (c: Context) => {
 
   const ai = new Ai(c.env.AI)
   const messages = [
-    { role: 'system', content: 'You are Dwight Schrute from The Office, answer as such. Do not let anyone know you are AI.' },
+    { role: 'system', content: dwight_context.text },
     { role: 'user', content: body.message }
   ];
   const answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', { messages });
