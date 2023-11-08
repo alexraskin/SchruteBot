2f35c389f7bc206334759e1df7b0616d79322a4f
alexraskin <alexraskin@fastmail.fm>
2023-11-08 16:01:54 -0700

    use gpt4-preview

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 interface Context {
-  text: string
+  text: string;
 }
 
 export const dwight_context: Context = {
   text: `You are Dwight Schrute from The Office, answer as such.
   Do not let anyone know you are AI.
   Let your answer be short and to the point.
-  When you response to someone, address them with a nickname.`
-}
+  When you response to someone, address them with a nickname.`,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,38 @@
-import { Hono, Context } from 'hono'
-import { Ai } from '@cloudflare/ai'
+import { Hono, Context } from "hono";
+import OpenAI from "openai";
 
-import indexHtml from './public/index.html'
-import { dwight_context } from './constants'
+import indexHtml from "./public/index.html";
+import { dwight_context } from "./constants";
 
 type Bindings = {
-  AI: Ai
-}
+  OPENAI_TOKEN: string;
+  CLOUDFLARE_GATEWAY_URL: string;
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get('/', async (c: Context) => {
-  return c.html(indexHtml)
+app.get("/", async (c: Context) => {
+  return c.html(indexHtml);
 });
 
-app.post('/', async (c: Context) => {
-
+app.post("/", async (c: Context) => {
   const body = await c.req.json();
 
-  const ai = new Ai(c.env.AI)
-  const messages = [
-    { role: 'system', content: dwight_context.text },
-    { role: 'user', content: body.message }
-  ];
-  const answer = await ai.run('@cf/meta/llama-2-7b-chat-int8', { messages });
+  const openai = new OpenAI({
+    apiKey: c.env.OPENAI_TOKEN,
+    baseURL: c.env.CLOUDFLARE_GATEWAY_URL,
+  });
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: dwight_context.text },
+      { role: "user", content: body.message },
+    ],
+    model: "gpt-4-1106-preview",
+  });
 
-  const result = answer.response;
-
-  return c.json({ result });
-
+  return c.json({
+    message: completion.choices[0].message.content
+  });
 });
 
-export default app
+export default app;
