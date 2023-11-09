f16269535ac96727dff4b8335c1dc787077cad0b
alexraskin <alexraskin@fastmail.fm>
2023-11-08 22:09:52 -0700

    rate limit

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,46 @@
-import { Hono, Context } from "hono";
+import { Hono, Context, Next } from "hono";
 import OpenAI from "openai";
+import { KVNamespace } from "@cloudflare/workers-types";
 
 import indexHtml from "./public/index.html";
 import { dwight_context } from "./constants";
 
 type Bindings = {
+  RATE_LIMIT_KV: KVNamespace;
   OPENAI_TOKEN: string;
   CLOUDFLARE_GATEWAY_URL: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.get("/", async (c: Context) => {
+app.use("*", async (c: Context, next: Next) => {
+
+  const MAX_REQUESTS_PER_MINUTE = 10;
+  const MINUTE = 60 * 1000;
+
+  const number = null;
+  const rateLimitKey = `rate-limit:${c.req.header('cf-connecting-ip')}`
+  const data = await c.env.RATE_LIMIT_KV.get(rateLimitKey, { count: number, expiry: number })
+
+  if (data) {
+    const json_data = JSON.parse(data)
+    if (Date.now() < json_data.expiry) {
+      if (json_data.count >= MAX_REQUESTS_PER_MINUTE) {
+        return c.json({ error: 'Rate limit exceeded' }, { status: 429 })
+      }
+      await c.env.RATE_LIMIT_KV.put(rateLimitKey, JSON.stringify({ count: json_data.count + 1, expiry: json_data.expiry }));
+    } else {
+        await c.env.RATE_LIMIT_KV.put(rateLimitKey, JSON.stringify({ count: 1, expiry: Date.now() + MINUTE }));
+    }
+  } else {
+    await c.env.RATE_LIMIT_KV.put(rateLimitKey, JSON.stringify({ count: 1, expiry: Date.now() + MINUTE }));
+  }
+
+  return await next()
+});
+
+
+app.get("/", async (c: Context, next: Next) => {
   return c.html(indexHtml);
 });
 
